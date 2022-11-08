e1ccf2847fc1c0032e38206c2ce342cae84757f9
e1b20023 <e1b20023@oit.ac.jp>
2022-11-08 15:45:05 +0900

    リポジトリの初期化

---
+++ src/main/java/team6/porker/PorkerApplication.java
@@ -0,0 +1,13 @@
+package team6.porker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team6/porker/PorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package team6.porker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
