36b9735bd373658f12136b05be8a4dc53c5d3013
b20083 <e1b20083@st.oit.ac.jp>
2022-11-22 15:44:38 +0900

    2層目のporkerの削除

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,17 @@
+CREATE TABLE poker2 (
+    id IDENTITY,
+    num INT NOT NULL,
+    suit VARCHAR NOT NULL
+);
+
+CREATE TABLE poker3 (
+    id IDENTITY,
+    num INT NOT NULL,
+    suit VARCHAR NOT NULL
+);
+
+CREATE TABLE poker4 (
+    id IDENTITY,
+    num INT NOT NULL,
+    suit VARCHAR NOT NULL
+);
