7e01cb37a8c0f20edea770a56b8f34215083b661
e1b20023 <e1b20023@oit.ac.jp>
2022-12-06 14:13:34 +0900

    トランプDBのID修正

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,69 +1,67 @@
-INSERT INTO poker4 (num,suit) VALUES (1,'heart');
-INSERT INTO poker4 (num,suit) VALUES (1,'dia');
-INSERT INTO poker4 (num,suit) VALUES (1,'club');
-INSERT INTO poker4 (num,suit) VALUES (1,'spade');
+INSERT INTO poker4 (id,num,suit) VALUES (101,1,'heart');
+INSERT INTO poker4 (id,num,suit) VALUES (201,1,'dia');
+INSERT INTO poker4 (id,num,suit) VALUES (301,1,'club');
+INSERT INTO poker4 (id,num,suit) VALUES (401,1,'spade');
 
-INSERT INTO poker4 (num,suit) VALUES (2,'heart');
-INSERT INTO poker4 (num,suit) VALUES (2,'dia');
-INSERT INTO poker4 (num,suit) VALUES (2,'club');
-INSERT INTO poker4 (num,suit) VALUES (2,'spade');
+INSERT INTO poker4 (id,num,suit) VALUES (102,2,'heart');
+INSERT INTO poker4 (id,num,suit) VALUES (202,2,'dia');
+INSERT INTO poker4 (id,num,suit) VALUES (302,2,'club');
+INSERT INTO poker4 (id,num,suit) VALUES (402,2,'spade');
 
-INSERT INTO poker4 (num,suit) VALUES (3,'heart');
-INSERT INTO poker4 (num,suit) VALUES (3,'dia');
-INSERT INTO poker4 (num,suit) VALUES (3,'club');
-INSERT INTO poker4 (num,suit) VALUES (3,'spade');
+INSERT INTO poker4 (id,num,suit) VALUES (103,3,'heart');
+INSERT INTO poker4 (id,num,suit) VALUES (203,3,'dia');
+INSERT INTO poker4 (id,num,suit) VALUES (303,3,'club');
+INSERT INTO poker4 (id,num,suit) VALUES (403,3,'spade');
 
-INSERT INTO poker4 (num,suit) VALUES (4,'heart');
-INSERT INTO poker4 (num,suit) VALUES (4,'dia');
-INSERT INTO poker4 (num,suit) VALUES (4,'club');
-INSERT INTO poker4 (num,suit) VALUES (4,'spade');
+INSERT INTO poker4 (id,num,suit) VALUES (104,4,'heart');
+INSERT INTO poker4 (id,num,suit) VALUES (204,4,'dia');
+INSERT INTO poker4 (id,num,suit) VALUES (304,4,'club');
+INSERT INTO poker4 (id,num,suit) VALUES (404,4,'spade');
 
-INSERT INTO poker4 (num,suit) VALUES (5,'heart');
-INSERT INTO poker4 (num,suit) VALUES (5,'dia');
-INSERT INTO poker4 (num,suit) VALUES (5,'club');
-INSERT INTO poker4 (num,suit) VALUES (5,'spade');
+INSERT INTO poker4 (id,num,suit) VALUES (105,5,'heart');
+INSERT INTO poker4 (id,num,suit) VALUES (205,5,'dia');
+INSERT INTO poker4 (id,num,suit) VALUES (305,5,'club');
+INSERT INTO poker4 (id,num,suit) VALUES (405,5,'spade');
 
-INSERT INTO poker4 (num,suit) VALUES (6,'heart');
-INSERT INTO poker4 (num,suit) VALUES (6,'dia');
-INSERT INTO poker4 (num,suit) VALUES (6,'club');
-INSERT INTO poker4 (num,suit) VALUES (6,'spade');
+INSERT INTO poker4 (id,num,suit) VALUES (106,6,'heart');
+INSERT INTO poker4 (id,num,suit) VALUES (206,6,'dia');
+INSERT INTO poker4 (id,num,suit) VALUES (306,6,'club');
+INSERT INTO poker4 (id,num,suit) VALUES (406,6,'spade');
 
-INSERT INTO poker4 (num,suit) VALUES (7,'heart');
-INSERT INTO poker4 (num,suit) VALUES (7,'dia');
-INSERT INTO poker4 (num,suit) VALUES (7,'club');
-INSERT INTO poker4 (num,suit) VALUES (7,'spade');
+INSERT INTO poker4 (id,num,suit) VALUES (107,7,'heart');
+INSERT INTO poker4 (id,num,suit) VALUES (207,7,'dia');
+INSERT INTO poker4 (id,num,suit) VALUES (307,7,'club');
+INSERT INTO poker4 (id,num,suit) VALUES (407,7,'spade');
 
-INSERT INTO poker4 (num,suit) VALUES (8,'heart');
-INSERT INTO poker4 (num,suit) VALUES (8,'dia');
-INSERT INTO poker4 (num,suit) VALUES (8,'club');
-INSERT INTO poker4 (num,suit) VALUES (8,'spade');
+INSERT INTO poker4 (id,num,suit) VALUES (108,8,'heart');
+INSERT INTO poker4 (id,num,suit) VALUES (208,8,'dia');
+INSERT INTO poker4 (id,num,suit) VALUES (308,8,'club');
+INSERT INTO poker4 (id,num,suit) VALUES (408,8,'spade');
 
-INSERT INTO poker4 (num,suit) VALUES (9,'heart');
-INSERT INTO poker4 (num,suit) VALUES (9,'dia');
-INSERT INTO poker4 (num,suit) VALUES (9,'club');
-INSERT INTO poker4 (num,suit) VALUES (9,'spade');
+INSERT INTO poker4 (id,num,suit) VALUES (109,9,'heart');
+INSERT INTO poker4 (id,num,suit) VALUES (209,9,'dia');
+INSERT INTO poker4 (id,num,suit) VALUES (309,9,'club');
+INSERT INTO poker4 (id,num,suit) VALUES (409,9,'spade');
 
-INSERT INTO poker4 (num,suit) VALUES (10,'heart');
-INSERT INTO poker4 (num,suit) VALUES (10,'dia');
-INSERT INTO poker4 (num,suit) VALUES (10,'club');
-INSERT INTO poker4 (num,suit) VALUES (10,'spade');
+INSERT INTO poker4 (id,num,suit) VALUES (110,10,'heart');
+INSERT INTO poker4 (id,num,suit) VALUES (210,10,'dia');
+INSERT INTO poker4 (id,num,suit) VALUES (310,10,'club');
+INSERT INTO poker4 (id,num,suit) VALUES (410,10,'spade');
 
+INSERT INTO poker4 (id,num,suit) VALUES (111,11,'heart');
+INSERT INTO poker4 (id,num,suit) VALUES (211,11,'dia');
+INSERT INTO poker4 (id,num,suit) VALUES (311,11,'club');
+INSERT INTO poker4 (id,num,suit) VALUES (411,11,'spade');
 
-INSERT INTO poker4 (num,suit) VALUES (11,'heart');
-INSERT INTO poker4 (num,suit) VALUES (11,'dia');
-INSERT INTO poker4 (num,suit) VALUES (11,'club');
-INSERT INTO poker4 (num,suit) VALUES (11,'spade');
+INSERT INTO poker4 (id,num,suit) VALUES (112,12,'heart');
+INSERT INTO poker4 (id,num,suit) VALUES (212,12,'dia');
+INSERT INTO poker4 (id,num,suit) VALUES (312,12,'club');
+INSERT INTO poker4 (id,num,suit) VALUES (412,12,'spade');
 
-
-INSERT INTO poker4 (num,suit) VALUES (12,'heart');
-INSERT INTO poker4 (num,suit) VALUES (12,'dia');
-INSERT INTO poker4 (num,suit) VALUES (12,'club');
-INSERT INTO poker4 (num,suit) VALUES (12,'spade');
-
-INSERT INTO poker4 (num,suit) VALUES (13,'heart');
-INSERT INTO poker4 (num,suit) VALUES (13,'dia');
-INSERT INTO poker4 (num,suit) VALUES (13,'club');
-INSERT INTO poker4 (num,suit) VALUES (13,'spade');
+INSERT INTO poker4 (id,num,suit) VALUES (113,13,'heart');
+INSERT INTO poker4 (id,num,suit) VALUES (213,13,'dia');
+INSERT INTO poker4 (id,num,suit) VALUES (313,13,'club');
+INSERT INTO poker4 (id,num,suit) VALUES (413,13,'spade');
 
 
 INSERT INTO score (hand,score) VALUES ('ロイヤルストレートフラッシュ',9);
