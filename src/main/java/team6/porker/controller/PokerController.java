3bc1af2d540b6a94ef5c2ab9aab50b37fdaf9729
b20083 <e1b20083@st.oit.ac.jp>
2022-12-13 16:44:28 +0900

    点数表示

--- src/main/java/team6/porker/controller/PokerController.java
+++ src/main/java/team6/porker/controller/PokerController.java
@@ -34,17 +34,17 @@ public class PokerController {
   @GetMapping("/poker4")
   public String poker41(ModelMap model) {
     ArrayList<Deck> ids = DeckMapper.selectAllByDeckid();
-    //user1.getHand().clear();
+    // user1.getHand().clear();
     for (int i = 0; i < ids.size(); ++i) {
       int rnd = (int) (Math.random() * (double) ids.size());
       int w = ids.get(i).getDeckid();
       ids.get(i).setDeckid(ids.get(rnd).getDeckid());
       ids.get(rnd).setDeckid(w);
     }
-    //Player user1 = new Player();
+    // Player user1 = new Player();
     user1.Distribute(ids);
     for (int i = 0; i < 5; i++) {
-      //user1copy.add(user1.get(i).getHand());
+      // user1copy.add(user1.get(i).getHand());
     }
 
     model.addAttribute("Hands", user1);
@@ -54,10 +54,13 @@ public class PokerController {
   @PostMapping("/poker4")
   public String poker42(ModelMap model) {
     int result;
+    int score;
 
     result = user1.getPokerHand(user1.getHand());
     String handname = user1.HandName(result);
     model.addAttribute("handname", handname);
+    score = user1.getScore();
+    model.addAttribute("score", score);
     return "poker4.html";
   }
 }
