5217f1770def11ef2f3802d9f9d4a8a420c54d2d
e1b20023 <e1b20023@oit.ac.jp>
2022-12-16 02:40:09 +0900

    カード交換機能の実装

--- src/main/java/team6/porker/controller/PokerController.java
+++ src/main/java/team6/porker/controller/PokerController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import team6.porker.model.DeckMapper;
 import team6.porker.model.Player;
@@ -15,6 +16,7 @@ import team6.porker.model.Deck;
 @Controller
 public class PokerController {
   Player user1 = new Player();
+  ArrayList<Deck> ids;
 
   @Autowired
   DeckMapper DeckMapper;
@@ -33,7 +35,7 @@ public class PokerController {
 
   @GetMapping("/poker4")
   public String poker41(ModelMap model) {
-    ArrayList<Deck> ids = DeckMapper.selectAllByDeckid();
+    ids = DeckMapper.selectAllByDeckid();
     // user1.getHand().clear();
     for (int i = 0; i < ids.size(); ++i) {
       int rnd = (int) (Math.random() * (double) ids.size());
@@ -65,6 +67,38 @@ public class PokerController {
     model.addAttribute("handname", handname);
     score = user1.getScore();
     model.addAttribute("score", score);
+    return "poker4.html";
+  }
+
+  @RequestMapping("/exchange")
+  public String exchange(ModelMap model,
+      @RequestParam(value = "h1", required = false) boolean h1,
+      @RequestParam(value = "h2", required = false) boolean h2,
+      @RequestParam(value = "h3", required = false) boolean h3,
+      @RequestParam(value = "h4", required = false) boolean h4,
+      @RequestParam(value = "h5", required = false) boolean h5) {
+
+    if (h1) {
+      user1.Exchange(0, ids);
+    }
+    if (h2) {
+      user1.Exchange(1, ids);
+    }
+    if (h3) {
+      user1.Exchange(2, ids);
+    }
+    if (h4) {
+      user1.Exchange(3, ids);
+    }
+    if (h5) {
+      user1.Exchange(4, ids);
+    }
+
+    model.addAttribute("Hand1", user1.getHand().get(0));
+    model.addAttribute("Hand2", user1.getHand().get(1));
+    model.addAttribute("Hand3", user1.getHand().get(2));
+    model.addAttribute("Hand4", user1.getHand().get(3));
+    model.addAttribute("Hand5", user1.getHand().get(4));
     return "poker4.html";
   }
 }
